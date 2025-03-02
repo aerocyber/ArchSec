e7fcdf130f555afa95e5a51034cfa8ef5f7a2493
aerocyber <aerocyberdev@proton.me>
2025-03-02 05:33:03 +0530

    Code cleanup, make the python file be generic to any arch iso and [t]ui refreshment

--- make_sec_boot_iso.py
+++ make_sec_boot_iso.py
@@ -3,8 +3,6 @@
 import os
 import pathlib
 import subprocess
-import requests
-
 from shutil import copyfile
 
 
@@ -16,31 +14,32 @@ def is_arch_based():
     """
 
     try:
-        subprocess.check_output(["pacman", "--version"])
-        return True
-    except subprocess.CalledProcessError:
-        pass
+        subprocess.run(["pacman", "--version"])
+    except Exception:
+        return False
 
-    return False
+    return True
 
 
-def grab_iso():
+# def grab_iso():
+#     """
+#         Download Arch ISO
+#     """
+#     url = "https://geo.mirror.pkgbuild.com/iso/2024.10.01/archlinux-x86_64.iso"
+#     r = requests.get(url, allow_redirects=True)
+#     f = open(url.split('/')[-1], 'wb')
+#     f.write(r.content)
+#     f.close()
+
+
+
+def extract_iso(file_path: str):
     """
-        Download Arch ISO
+        Extract the iso to get the bootloader, kernel and other required files.\
+        Args:
+            file_path: str: Path to the arch iso image
     """
-    url = "https://geo.mirror.pkgbuild.com/iso/2024.10.01/archlinux-x86_64.iso"
-    r = requests.get(url, allow_redirects=True)
-    f = open(url.split('/')[-1], 'wb')
-    f.write(r.content)
-    f.close()
-
-
-
-def extract_iso():
-    """
-        Extract the iso to get the bootloader, kernel and other required files.
-    """
-    cmd = "osirrox -indev ../archlinux-x86_64.iso -extract_boot_images ./ -extract /EFI/BOOT/BOOTx64.EFI grubx64.efi -extract /shellx64.efi shellx64.efi -extract /arch/boot/x86_64/vmlinuz-linux vmlinuz-linux"
+    cmd = f"osirrox -indev ../{file_path} -extract_boot_images ./ -extract /EFI/BOOT/BOOTx64.EFI grubx64.efi -extract /shellx64.efi shellx64.efi -extract /arch/boot/x86_64/vmlinuz-linux vmlinuz-linux"
     ret = subprocess.run(cmd, shell=True, capture_output=True)
     if ret.returncode != 0:
         print(f"Error: {ret.stderr.decode('utf-8')}")
@@ -54,7 +53,8 @@ def extract_iso():
 
 def get_shim_bin():
     """
-        Get the shim binaries from `shim-signed`
+        Get the shim binaries from `shim-signed`.
+        The binaries MUST be located in `/usr/share/shim-signed/`
     """
     cmd1 = "cp /usr/share/shim-signed/shimx64.efi BOOTx64.EFI"
     cmd2 = "cp /usr/share/shim-signed/mmx64.efi ./"
@@ -157,11 +157,14 @@ def copy_to_image():
     return 0
 
 
-def repack_iso():
+def repack_iso(old_file_path: str, new_file_path: str):
     """
         Repack the ISO file.
+        Args:
+            old_file_path: str: Path to the Arch ISO Image
+            new_file_path: str: Path to where the modified ISO is to be created. Must be a path to ISO file and not a directory.
     """
-    cmd = "xorriso -indev ../archlinux-x86_64.iso -outdev archlinux-secure-boot-shim-x86_64.iso -map vmlinuz-linux /arch/boot/x86_64/vmlinuz-linux -map_l ./ / shellx64.efi MOK.cer -- -map_l ./ /EFI/BOOT/ BOOTx64.EFI grubx64.efi mmx64.efi -- -boot_image any replay -append_partition 2 0xef eltorito_img2_uefi.img"
+    cmd = f"xorriso -indev ../{old_file_path} -outdev {new_file_path} -map vmlinuz-linux /arch/boot/x86_64/vmlinuz-linux -map_l ./ / shellx64.efi MOK.cer -- -map_l ./ /EFI/BOOT/ BOOTx64.EFI grubx64.efi mmx64.efi -- -boot_image any replay -append_partition 2 0xef eltorito_img2_uefi.img"
 
     ret = subprocess.run(cmd, shell=True, capture_output=True)
     if ret.returncode != 0:
@@ -177,37 +180,84 @@ def create_mok_keys():
     cmd1 = 'openssl req -x509 -newkey rsa:2048 -keyout MOK.key -out MOK.crt -subj "/CN=Aero/"' # Replace Aero with your name!
     cmd2 = "openssl x509 -in MOK.crt -out MOK.cer -outform DER"
 
+    ret1 = subprocess.run(cmd1, shell=True, capture_output=True)
+    if ret1.returncode != 0:
+        print(f"Error: {ret1.stderr.decode('utf-8')}")
+        exit(1)
+    else:
+        print(f"[INFO]: {ret1.stdout.decode('utf-8')}\n\n")
+
+    ret2 = subprocess.run(cmd2, shell=True, capture_output=True)
+    if ret2.returncode != 0:
+        print(f"Error: {ret2.stderr.decode('utf-8')}")
+        exit(1)
+    else:
+        print(f"[INFO]: {ret2.stdout.decode('utf-8')}\n\n")
+
+def banner():
+    banner_ = r"""
+    _             _     ____            
+   / \   _ __ ___| |__ / ___|  ___  ___ 
+  / _ \ | '__/ __| '_ \\___ \ / _ \/ __|
+ / ___ \| | | (__| | | |___) |  __/ (__ 
+/_/   \_\_|  \___|_| |_|____/ \___|\___|
+                                         
+"""
+    print(banner_)
+
 
 if __name__ == '__main__':
     if not is_arch_based():
         print("This is not an Arch based system. Exiting...")
         exit(1)
 
+    banner()
+
     print("Arch or Arch based system found. Ensure you have the following packages installed:")
     print("shim-signed - AUR")
     print("libisoburn - REPO")
     print("mtools - REPO")
 
+    x = input("Continue? [Y/n]:\t")
+    if not (x.upper() == 'Y' or x.upper() == "YES"):
+        print("Exiting...")
+        exit(0)
+
     cwd = os.getcwd()
     os.mkdir(f"{cwd}/archsec")
 
-    if not pathlib.Path(f"{cwd}/archlinux-x86_64.iso").exists():
-        grab_iso()
+    archIso = input("Enter path to the Arch or Arch based ISO:\t")
+    newArchIso = input("Enter path to the new ISO file. The file name must end with .iso and will be created if this script runs successfully:\t")
+
+    if not pathlib.Path(archIso).exists():
+        print("[ERROR]: File Not Found.\nExiting...")
+        exit(1)
+
+    mok_key = input("Enter path to MOK.key:\t")
+    mok_cer = input("Enter path to MOK.cer:\t")
+    mok_crt = input("Enter path to MOK.crt:\t")
+
+    key_created = False
 
     if not (pathlib.Path(f"{cwd}/MOK.key").exists() or pathlib.Path(f"{cwd}/MOK.crt").exists() or pathlib.Path(f"{cwd}/MOK.cer").exists()):
+        key_created = True
         create_mok_keys()
 
+    if key_created == False and not (mok_cer.endswith('cer') and mok_crt('crt') and mok_key.endswith('key')):
+        print("Invalid MOK extensions.\nExiting...")
+        exit(1)
+    
     os.chdir(f"{cwd}/archsec")
-    copyfile("../MOK.cer", './MOK.cer')
-    copyfile("../MOK.crt", './MOK.crt')
-    copyfile("../MOK.key", './MOK.key')
+    copyfile(mok_cer, './MOK.cer')
+    copyfile(mok_crt, './MOK.crt')
+    copyfile(mok_key, './MOK.key')
 
-    extract_iso()
+    extract_iso(archIso)
     get_shim_bin()
     sign_with_sbsigntools()
     copy_to_image()
-    repack_iso()
+    repack_iso(archIso, newArchIso)
     
     
-    print(f"Done... Check the iso file: archlinux-secure-boot-shim-x86_64.iso at: {cwd}/archsec")
+    print(f"Done... Check the iso file at: {newArchIso}")
     os.chdir(cwd)
